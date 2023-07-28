6001fdbb66e7d8d09bcd2dbef5ed57a290294ae9
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-07-28 13:44:42 +0900

    [bookers2-debug]途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-  end
-
-  def index
-    @books = Book.all
+    @new_book = Book.new
+    @user = User.find(@book.user_id)
   end
 
   def create
@@ -15,8 +13,13 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
@@ -32,15 +35,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+  ensure_correct_user
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      @books = @user.books
+      render :edit
     end
   end
 
